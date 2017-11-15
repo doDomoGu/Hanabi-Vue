41339fec6fa8b21eab67e1abcfcf0a9707b0b83b
doDomoGu <kaka_71936410@qq.com>
2017-11-15 17:41:02 +0800

    rest start

--- backend/urls.py
+++ backend/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from django.contrib import admin
+from django.conf.urls import url, include
+#from django.contrib import admin
+from rest_framework import routers
+from backend.api import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    #url(r'^admin/', admin.site.urls),
+    #url(r'^api-token-auth/', views.obtain_auth_token),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
